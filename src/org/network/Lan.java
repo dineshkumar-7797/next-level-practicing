2cb3baf9b86d1817bddb2b79e939135975ef46b5
dineshkumar-7797 <dineshkumaroff7p@gmail.com>
2022-09-23 12:17:57 +0530

    update by c

--- src/org/network/Lan.java
+++ src/org/network/Lan.java
@@ -1,8 +1,8 @@
 package org.network;
 
 public class Lan {
-	
-public void lanName() {
+
+	public void lanName() {
 
 		System.out.println("Lan Name : QWERTY");
 		System.out.println("Lan Name : QWERTY + c");
